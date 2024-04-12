e40a5f8ae9685955fa65bea4551219ae4cfa98fe
Devilal panchal <devilalpanchal551@gmail.com>
2024-04-12 18:22:15 +0530

    add

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 
 const url = "https://restcountries.com/v3.1/all"
 
-fetch('https://restcountries.com/v3.1/name/{name}')
-.then((Response)=>Response.json()
+fetch('https://restcountries.com/v3.1/all')
+    .then((Response) => Response.json()
 
+    )
+    .then((data) => {
+        let countries = data
+        console.log(countries)
+        let result = countries.map((item) => {
+            return `
+        <div class="card">
+        <img class="imgOfcountry" src="${item.flags.png}" alt="">
+        <div class="other">
+        <h3 class="countryName">${item.name.common}</h3>
+        <p class="population">Population:${item.population}</p>
+        <p class="region">Region:${item.region}</p>
+        <p class="capital">Capital:${item.capital}</p>
+    </div>
+</div>
 
-)
-.then((data)=>{
-    console.log(data)
-}).catch((err)=>{
-    console.log('Eroor');
-})
+        `
+        });
+        mainSection.innerHTML = result.join("")
+
+    }).catch((err) => {
+        console.log('Eroor');
+    })
